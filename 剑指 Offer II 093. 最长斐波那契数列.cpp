5909f8b6005acd3ab9272742349bbcb23500da69
sjsfwch <sjsfwch@vip.qq.com>
2021-09-05 11:19:00 +0800

    2021.09.05

--- 剑指 Offer II 093. 最长斐波那契数列.cpp
+++ 剑指 Offer II 093. 最长斐波那契数列.cpp
@@ -2,5 +2,12 @@
 
 class Solution {
    public:
-    int lenLongestFibSubseq(vector<int>& arr) {}
+    int lenLongestFibSubseq(vector<int>& arr) {
+        int n=arr.size();
+        // dp[i][j]为以arr[i],arr[j]为最后两个元素的子序列长度
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+
+
+    }
 };
